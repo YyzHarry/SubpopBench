906cb350000b802f266bb7830c8009be5e5c2bdb
YyzHarry <1400012996@pku.edu.cn>
2023-02-23 09:41:30 -0500

    minor updates

--- subpopbench/scripts/preprocess_mimic_notes.py
+++ subpopbench/scripts/preprocess_mimic_notes.py
@@ -1,30 +1,31 @@
 # Adapted from https://github.com/irenetrampoline/mimic-disparities/tree/master
 
+import psycopg2
+import random
+import pickle
 import numpy as np
 import pandas as pd
-import psycopg2
-import json
-import os 
-import random
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from pathlib import Path
-import pickle
+
 
 def save(df, features, vocab, meta, path):
     df.to_pickle(path/'cohort.pkl')
     np.save(path/'features.npy', features)
     pickle.dump(vocab, (path/'vocab.pkl').open('wb'))
     pickle.dump(meta, (path/'meta.pkl').open('wb'))
-    
-def split(df, eval_weight = 1, target_name = 'target'):
+
+
+def split(df, eval_weight=1, target_name='target'):
     n_splits_train = 5
     idx_train = df[df.fold_id == 'train'].index
-    skf = StratifiedKFold(n_splits = n_splits_train + eval_weight, random_state = 42, shuffle = True)
+    skf = StratifiedKFold(n_splits=n_splits_train + eval_weight, random_state=42, shuffle=True)
     for c, (_, fold_index) in enumerate(skf.split(idx_train, df.loc[idx_train, target_name])):
         df.loc[idx_train[fold_index], 'fold_id'] = str(c)
     df.loc[df.fold_id.isin(list(map(str, range(n_splits_train, n_splits_train + eval_weight, 1)))), 'fold_id'] = 'eval'
     return df   
+
 
 def bin_age(age):
     bins = [
@@ -37,12 +38,12 @@ def bin_age(age):
     ]
 
     for c, i in enumerate(bins):
-        if (age >= i[0] and age < i[1]) or (c == len(bins) -1 and age == i[1]):
+        if (i[0] <= age < i[1]) or (c == len(bins) - 1 and age == i[1]):
             return f'{i[0]}-{i[1]}'
 
 
 output_dir = Path('/data/healthy-ml/scratch/haoran/results/subpop_bench/data') # TODO: change
-output_dir.mkdir(parents = True, exist_ok = True)
+output_dir.mkdir(parents=True, exist_ok=True)
 
 random.seed(22891)
 
@@ -56,7 +57,8 @@ con = psycopg2.connect(dbname=dbname, user=sqluser, host = dbname, password = 'p
 cur = con.cursor()
 cur.execute('SET search_path to ' + schema_name)
 
-#========helper function for imputing missing values 
+
+# ======== helper function for imputing missing values
 
 def replace(group):
     """
@@ -69,7 +71,7 @@ def replace(group):
     return group
 
 
-#========get the icu details 
+# ======== get the icu details
 
 # this query extracts the following:
 #   Unique ids for the admission, patient and icu stay 
@@ -127,15 +129,15 @@ WHERE adm.has_chartevents_data = 1
 ORDER BY ie.subject_id, adm.admittime, ie.intime;
 """
 
-den = pd.read_sql_query(denquery,con)
+den = pd.read_sql_query(denquery, con)
 
-#----drop patients with less than 48 hour 
+# ---- drop patients with less than 48 hour
 den['los_icu_hr'] = (den.outtime - den.intime).astype('timedelta64[h]')
 den = den[(den.los_icu_hr >= 48)]
-den = den[(den.age<300)]
-den.drop('los_icu_hr', 1, inplace = True)
+den = den[(den.age < 300)]
+den.drop('los_icu_hr', 1, inplace=True)
 
-#----clean up
+# ---- clean up
 
 # micu --> medical 
 # csru --> cardiac surgery recovery unit 
@@ -154,9 +156,9 @@ den.ethnicity.loc[~(den.ethnicity.str.contains('|'.join(['white', 'black'])))] =
 # den = pd.concat([den, pd.get_dummies(den['ethnicity'], prefix='eth')], 1)
 # den = pd.concat([den, pd.get_dummies(den['admission_type'], prefix='admType')], 1)
 
-den.drop(['diagnosis', 'hospstay_seq', 'los_icu','icustay_seq', 'admittime', 'dischtime','los_hospital', 'intime', 'outtime', 'first_careunit'], 1, inplace =True) 
+den.drop(['diagnosis', 'hospstay_seq', 'los_icu', 'icustay_seq', 'admittime', 'dischtime', 'los_hospital', 'intime', 'outtime', 'first_careunit'], 1, inplace=True)
 
-den = den[(den['adult_icu']==1)].dropna()
+den = den[(den['adult_icu'] == 1)].dropna()
 
 notesquery = \
 """
@@ -174,9 +176,9 @@ group by fin.subject_id, fin.hadm_id, fin.icustay_id
 order by fin.subject_id, fin.hadm_id, fin.icustay_id; 
 """
 
-notes48 = pd.read_sql_query(notesquery,con) 
+notes48 = pd.read_sql_query(notesquery, con)
 
-output_df = notes48.merge(den,on=['subject_id', 'hadm_id', 'icustay_id'], how='inner')
+output_df = notes48.merge(den, on=['subject_id', 'hadm_id', 'icustay_id'], how='inner')
 
 print('notes48:', len(notes48))
 print('demographics:', len(den))
@@ -184,17 +186,17 @@ print('merged:', len(output_df))
 
 output_df['age_group'] = output_df.age.apply(bin_age)
 
-train_val_inds, test_inds = train_test_split(output_df.index, test_size = 0.25, random_state = 42)
+train_val_inds, test_inds = train_test_split(output_df.index, test_size=0.25, random_state=42)
 output_df['fold_id'] = None
 output_df.loc[train_val_inds, 'fold_id'] = 'train'
 output_df.loc[test_inds, 'fold_id'] = 'test'
-output_df = split(output_df, target_name = 'mort_hosp')
+output_df = split(output_df, target_name='mort_hosp')
 
 n_tfidf_features = 10000
-tfidf = TfidfVectorizer(max_features = n_tfidf_features).fit(output_df.query('fold_id in ["0", "1", "2", "3", "4"]').chartext.values)
+tfidf = TfidfVectorizer(max_features=n_tfidf_features).fit(output_df.query('fold_id in ["0", "1", "2", "3", "4"]').chartext.values)
 features = tfidf.transform(output_df.chartext.values)
 
-output_df = output_df.reset_index().rename(columns = {'index': 'array_index'})
+output_df = output_df.reset_index().rename(columns={'index': 'array_index'})
 
 vocab = tfidf.get_feature_names()
 
@@ -204,5 +206,5 @@ meta = {
         'vocab': vocab
     }
 
-(output_dir/'mimic_notes').mkdir(exist_ok = True)
+(output_dir/'mimic_notes').mkdir(exist_ok=True)
 save(output_df, features, vocab, meta, output_dir/'mimic_notes')
